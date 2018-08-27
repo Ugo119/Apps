e3d4aa59962b240b76d1cedb25e2e7b3efaf7197
ugo119 <onuoha.ugochukwu@yahoo.com>
2018-08-27 14:19:19 +0100

    first JSON parsing tutorial

--- app/src/main/java/com/example/ugo/quakereport/MainActivity.java
+++ app/src/main/java/com/example/ugo/quakereport/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Quake> quakes = new ArrayList<Quake>();
+       /* ArrayList<Quake> quakes = new ArrayList<Quake>();
         quakes.add(new Quake("7.2", "San Fransisco", "Feb 2, 2016"));
         quakes.add(new Quake("6.1", "London", "Jul 20, 2015"));
         quakes.add(new Quake("3.9", "Tokyo", "Nov 10, 2014"));
@@ -22,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
         quakes.add(new Quake("2.8", "Moscow", "Jan 31, 2012"));
         quakes.add(new Quake("4.9", "Rio De Janeiro", "Aug 19, 2012"));
         quakes.add(new Quake("1.6", "Paris", "Oct 30, 2011"));
-
+        */
+        ArrayList<Quake> quakes = QueryUtils.extractEarthquakes();
         //Create a new adapter that takes the list of earthquakes as input
         QuakeAdapter adapter = new QuakeAdapter(this,quakes);
 
