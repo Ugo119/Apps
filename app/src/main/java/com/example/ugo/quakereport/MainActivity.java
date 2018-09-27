94a23b0e5fcbcd9e76cf116808864f1c7c5b607c
ugo119 <onuoha.ugochukwu@yahoo.com>
2018-09-27 12:28:30 +0100

    Multithread done before Loaders

--- app/src/main/java/com/example/ugo/quakereport/MainActivity.java
+++ app/src/main/java/com/example/ugo/quakereport/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ugo.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,39 +13,39 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    /* Adapter for the list of earthquakes */
+    private QuakeAdapter mAdapter;
+
+    /** URL for earthquake data from the USGS dataset */
+    private static final String USGS_REQUEST_URL =
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=10";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       /* ArrayList<Quake> quakes = new ArrayList<Quake>();
-        quakes.add(new Quake("7.2", "San Fransisco", "Feb 2, 2016"));
-        quakes.add(new Quake("6.1", "London", "Jul 20, 2015"));
-        quakes.add(new Quake("3.9", "Tokyo", "Nov 10, 2014"));
-        quakes.add(new Quake("5.4", "Mexico City", "May 3, 2014"));
-        quakes.add(new Quake("2.8", "Moscow", "Jan 31, 2012"));
-        quakes.add(new Quake("4.9", "Rio De Janeiro", "Aug 19, 2012"));
-        quakes.add(new Quake("1.6", "Paris", "Oct 30, 2011"));
-        */
-        ArrayList<Quake> quakes = QueryUtils.extractEarthquakes();
-        //Create a new adapter that takes the list of earthquakes as input
-        final QuakeAdapter adapter = new QuakeAdapter(this,quakes);
 
         //Find a reference to the {@Link ListView} in the Layout
         ListView listView = (ListView) findViewById(R.id.list_list);
 
-        //Ste the adapter on the ListView so the list can be populated in the user interface
-        listView.setAdapter(adapter);
+        // Create a new adapter that takes an empty list of earthquakes as input
+        mAdapter = new QuakeAdapter(this, new ArrayList<Quake>());
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        listView.setAdapter(mAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
 
                 // Find the current earthquake that was clicked on
-                Quake currentEarthquake = adapter.getItem(position);
+                Quake currentEarthquake = mAdapter.getItem(position);
 
                 // Convert the String URL into a URI object (to pass into the Intent constructor)
                 Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
@@ -57,5 +58,48 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Start the AsyncTask to fetch the earthquake data
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
     }
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<Quake>> {
+
+        @Override
+        protected List<Quake> doInBackground(String... urls) {
+
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+
+            List<Quake> result = QueryUtils.fetchEarthquakeData(urls[0]);
+            return result;
+
+        }
+
+        /*
+         * This method runs on the main UI thread after the background work has been
+         * completed. This method receives as input, the return value from the doInBackground()
+         * method. First we clear out the adapter, to get rid of earthquake data from a previous
+         * query to USGS. Then we update the adapter with the new list of earthquakes,
+         * which will trigger the ListView to re-populate its list items.
+         */
+
+        @Override
+        protected void onPostExecute(List<Quake> data) {
+
+            // Clear the adapter of previous earthquake data
+            mAdapter.clear();
+
+            // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
+            // data set. This will trigger the ListView to update.
+            if (data != null && !data.isEmpty()) {
+                mAdapter.addAll(data);
+            }
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/ugo/quakereport/Quake.java
+++ app/src/main/java/com/example/ugo/quakereport/Quake.java
@@ -1,21 +1,47 @@
 package com.example.ugo.quakereport;
 
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Quake {
     private double mMag;
     private String mLocation;
     private long mTimeInMilliseconds;
     private String mUrl;
-    Quake(double mag, String location, long timeInMilliseconds, String url){
+
+
+    Quake(double mag, String location, long timeInMilliseconds, String url) {
         mMag = mag;
         mLocation = location;
         mTimeInMilliseconds = timeInMilliseconds;
         mUrl = url;
-     }
-     public double getMagnitude(){return mMag;}
-     public String getLocation(){return mLocation;}
-     public long getTimeInMilliseconds(){return mTimeInMilliseconds;}
+    }
+
+    public double getMagnitude() {
+        return mMag;
+    }
+
+    public String getLocation() {
+        return mLocation;
+    }
+
+    public long getTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+
     /*
      * Returns the website URL to find more information about the earthquake.
      */
-    public String getUrl() {return mUrl;}
+    public String getUrl() {
+        return mUrl;
+    }
 }
+
+
+
+
+
